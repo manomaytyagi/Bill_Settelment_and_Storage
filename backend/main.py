0c4fcd1478f75fa3a5b8c88ef0ac3649c8829603
Manomay-Intacting <manomay.t@intacting.in>
2026-07-03 15:50:46 +0530

    Connect Supabase

--- backend/main.py
+++ backend/main.py
@@ -21,22 +21,35 @@ def get_health():
 @app.post("/submit")
 def submit_form(proof: UploadFile, bill: UploadFile, project: str = Form(...), vendor: str = Form(...), amount: int = Form(...)):
     proof_bytes = proof.file.read()
+    proof_path = f"{uuid.uuid4()}_{proof.filename}"
     response_proof = (
         supabase.storage
         .from_("bills")
         .upload(
             file=proof_bytes,
-            path=f"{uuid.uuid4()}_{proof.filename}",
+            path=proof_path,
             file_options={"cache-control": "3600", "upsert": "false"}
         )
     )
+    
     bill_bytes = bill.file.read()
+    bill_path = f"{uuid.uuid4()}_{bill.filename}"
     response_bill = (
         supabase.storage
         .from_("bills")
         .upload(
             file=bill_bytes,
-            path=f"{uuid.uuid4()}_{bill.filename}",
+            path=bill_path,
             file_options={"cache-control": "3600", "upsert": "false"}
         )
     )
+    proof_url = supabase.storage.from_("bills").get_public_url(proof_path)
+    bill_url = supabase.storage.from_("bills").get_public_url(bill_path)
+    
+    supabase.table("submissions").insert({
+        "payment_proof": proof_url,
+        "bill": bill_url,
+        "project": project,
+        "vendor": vendor,
+        "amount": amount
+    }).execute()
